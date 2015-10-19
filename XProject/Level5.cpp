1517b8e583e2651c747a6cc842f553c4281a3b9f
Roye Li <royetest02@hotmail.com>
2015-10-18 22:08:19 -0700

    spiralOrder

--- XProject/Level5.cpp
+++ XProject/Level5.cpp
@@ -50,9 +50,129 @@ void testMaxSubArray()
     cout << maxSubArray(t) << endl;
 }
 
+#pragma mark - spiralOrder
+
+//Given a matrix of m x n elements (m rows, n columns), return all elements of the matrix in spiral order.
+//
+//For example,
+//Given the following matrix:
+//
+//[
+// [ 1, 2, 3 ],
+// [ 4, 5, 6 ],
+// [ 7, 8, 9 ]
+// ]
+//You should return [1,2,3,6,9,8,7,4,5].
+vector<int> spiralOrder(vector<vector<int>>& matrix)
+{
+    vector<int> r;
+    
+    int sizeH = matrix.size();
+    if( sizeH == 0 )
+        return r;
+    
+    int sizeW = matrix[0].size();
+    if( sizeW == 0 )
+        return r;
+    
+    struct Point
+    {
+        int x;
+        int y;
+    };
+    
+    Point start{ 0, 0 };
+    Point innerSize { sizeW, sizeH };
+    int n = (min( sizeW, sizeH ) - 1 )/ 2;
+    for( int i = 0; i <= n; i++ )
+    {
+        Point current = start;
+        r.push_back(matrix[current.y][current.x]);
+        
+        // Go right
+        while( current.x + 1 < start.x + innerSize.x )
+        {
+            current.x++;
+            r.push_back(matrix[current.y][current.x]);
+        }
+        
+        // Go down
+        bool goDown = false;
+        while( current.y + 1 < start.y + innerSize.y )
+        {
+            goDown = true;
+            current.y++;
+            r.push_back(matrix[current.y][current.x]);
+        }
+        
+        // Go left
+        bool goLeft = false;
+        while( goDown && current.x - 1 >= start.x )
+        {
+            goLeft = true;
+            current.x--;
+            r.push_back(matrix[current.y][current.x]);
+        }
+        
+        // Go Up
+        while( goLeft && current.y - 1 > start.y )
+        {
+            current.y--;
+            r.push_back(matrix[current.y][current.x]);
+        }
+        
+        start.x += 1;
+        start.y += 1;
+        innerSize.x -= 2;
+        innerSize.y -= 2;
+    }
+    
+    return r;
+}
+
+void testSpiralOrder()
+{
+    vector< vector<vector<int>>> ms =
+    {
+        {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        },
+        {
+            {1, 2},
+            {3, 4},
+            {5, 6}
+        },
+        {
+            {1, 2, 3},
+            {4, 5, 6}
+        },
+        {
+            {1, 2, 3}
+        },
+        {
+            {1},
+            {2},
+            {3}
+        }
+    };
+    
+    for( auto& m : ms )
+    {
+        vector<int> r = spiralOrder( m );
+        
+        for( int& i : r )
+        {
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+}
+
 #pragma mark - run
 
 void Level5::Run()
 {
-    testMaxSubArray();
+    testSpiralOrder();
 }
