f157c5f121cdf63ec07fa4669a20be0742b52ab8
Roye Li <royetest02@hotmail.com>
2015-10-29 22:27:00 -0700

    CanJump
    DFS apparently is too slow , failed on case
    [2,0,6,9,8,4,5,0,8,9,1,2,9,6,8,8,0,6,3,1,2,2,1,2,6,5,3,1,2,2,6,4,2,4,3,0,0,0,3,8,2,4,0,1,2,0,1,4,6,5,8,0,7,9,3,4,6,6,5,8,9,3,4,3,7,0,4,9,0,9,8,4,3,0,7,7,1,9,1,9,4,9,0,1,9,5,7,7,1,5,8,2,8,2,6,8,2,2,7,5,1,7,9,6]

--- XProject/Level5.cpp
+++ XProject/Level5.cpp
@@ -170,9 +170,93 @@ void testSpiralOrder()
     }
 }
 
+#pragma mark - canJump
+//Given an array of non-negative integers, you are initially positioned at the first index of the array.
+//
+//Each element in the array represents your maximum jump length at that position.
+//
+//Determine if you are able to reach the last index.
+//
+//For example:
+//A = [2,3,1,1,4], return true.
+//
+//A = [3,2,1,0,4], return false.
+
+class Node
+{
+public:
+    int v;
+    vector< Node* > nexts;
+};
+
+bool _canJump( Node* node, int expect )
+{
+    if( node == nullptr )
+        return false;
+    
+    if( node->v == expect )
+        return true;
+    
+    for( Node* subNodes : node->nexts )
+    {
+        if( _canJump( subNodes, expect ) )
+            return true;
+    }
+    
+    return false;
+}
+
+bool canJump(vector<int>& nums)
+{
+    if( nums.size() == 0 )
+        return false;
+    
+    vector< unique_ptr< Node > > nodes;
+    for( int i = 0; i < nums.size(); i++ )
+    {
+        unique_ptr< Node > node( new Node );
+        node->v = i;
+        nodes.push_back( std::move( node ) );
+    }
+    
+    for( int i = 0; i < nums.size(); i++ )
+    {
+        vector< weak_ptr< Node > > nexts;
+        for( int j = 0; j < nums[i]; j++ )
+        {
+            if( i + j + 1 < nums.size() )
+            {
+                nodes[i]->nexts.push_back( nodes[ i + j + 1 ].get() );
+            }
+        }
+    }
+
+
+    return _canJump( nodes[0].get(), nums.size() - 1 );
+}
+
+void testCanJump()
+{
+    vector< vector< int > > tests =
+    {
+        {2,0,6,9,8,4,5,0,8,9,1,2,9,6,8,8,0,6,3,1,2,2,1,2,6,5,3,1,2,2,6,4,2,4,3,0,0,0,3,8,2,4,0,1,2,0,1,4,6,5,8,0,7,9,3,4,6,6,5,8,9,3,4,3,7,0,4,9,0,9,8,4,3,0,7,7,1,9,1,9,4,9,0,1,9,5,7,7,1,5,8,2,8,2,6,8,2,2,7,5,1,7,9,6},
+        { 3,2,1,0,4 },
+        { 2,3,1,1,4 },
+        { },
+        { 1 },
+        { 0, 1 },
+        { 1, 0 }
+    };
+    
+    for( vector< int >& v : tests )
+    {
+        cout << ( canJump( v ) ? "true" : "false" ) << endl;
+    }
+}
+
 #pragma mark - run
 
 void Level5::Run()
 {
-    testSpiralOrder();
+    testCanJump();
 }
