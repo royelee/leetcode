e4ae698a713ebed5f1bdd1c7056d04500bef98ff
Roye Li <royetest02@hotmail.com>
2015-11-02 22:00:25 -0800

    lengthOfLastWord
    Failed twice.
    
    I don't what I'm thinking, but obviously my way is stupid, should be trim the string and find it from the last!!!!

--- XProject/Level5.cpp
+++ XProject/Level5.cpp
@@ -236,9 +236,58 @@ void testCanJump()
     }
 }
 
+#pragma mark - lengthOfLastWord
+
+//Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
+//
+//If the last word does not exist, return 0.
+//
+//Note: A word is defined as a character sequence consists of non-space characters only.
+//
+//For example,
+//Given s = "Hello World",
+//return 5.
+int lengthOfLastWord(string s)
+{
+    stringstream ss;
+    string stack;
+    for( int i = 0; i < s.size(); i++ )
+    {
+        if( s[i] != ' ' )
+        {
+            ss << s[i];
+        }
+        else
+        {
+            string l = ss.str();
+            if( l.size() > 0 )
+                stack = ss.str();
+
+            ss.str("");
+        }
+    }
+    
+    string end = ss.str();
+    return end.empty() ? stack.size() : end.size();
+}
+
+void testLengthOfLastWord()
+{
+    string s = "Hello World";
+    cout << lengthOfLastWord(s) << endl;
+    
+    // failed
+    s = "a ";
+    cout << lengthOfLastWord(s) << endl;
+    
+    s = "b   a    ";
+    cout << lengthOfLastWord(s) << endl;
+}
+
+
 #pragma mark - run
 
 void Level5::Run()
 {
-    testCanJump();
+    testLengthOfLastWord();
 }
