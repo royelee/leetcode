fcbff8a64e0f668c55a1dbc391a3220514e00643
Roye Li <royetest02@hotmail.com>
2015-11-21 20:51:11 -0800

    setZeroes
    slow version using O(m+n) space.

--- XProject/Level6.cpp
+++ XProject/Level6.cpp
@@ -309,11 +309,70 @@ void testSimplifyPath()
     for( auto& p : tests )
     {
         cout << (simplifyPath(p.first) == p.second) << endl;
-    }}
+    }
+}
+
+#pragma mark - setZeroes
+//Given a m x n matrix, if an element is 0, set its entire row and column to 0. Do it in place.
+//
+// Follow up:
+//Did you use extra space?
+//A straight forward solution using O(mn) space is probably a bad idea.
+//A simple improvement uses O(m + n) space, but still not the best solution.
+//Could you devise a constant space solution?
+void setZeroes(vector<vector<int>>& matrix)
+{
+    int m = matrix.size();
+    int n = matrix[0].size();
+    
+    vector<bool> f( m + n );
+    
+    for( int i = 0; i < m; i++ )
+    {
+        for( int j = 0; j < n; j++ )
+        {
+            if( matrix[i][j] == 0 )
+            {
+                f[i] = true;
+                f[m+j] = true;
+            }
+        }
+    }
+    
+    for( int i = 0; i < f.size(); i++ )
+    {
+        if( f[i] )
+        {
+            if( i < m ) // row
+            {
+                for( int t = 0; t < n; t++ )
+                {
+                    matrix[i][t] = 0;
+                }
+            }
+            else    // col
+            {
+                for( int t = 0; t < m; t++ )
+                {
+                    matrix[t][i-m] = 0;
+                }
+            }
+        }
+    }
+}
+
+void testSetZeroes()
+{
+    vector<vector<int>> m = {
+        { 1, 0 }
+    };
+    setZeroes( m );
+}
+
 
 #pragma mark - run
 
 void Level6::Run()
 {
-    testSimplifyPath();
+    testSetZeroes();
 }
