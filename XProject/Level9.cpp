a28999b26bea002a1676be4ba193c174c5064c35
Roye Li <royetest02@hotmail.com>
2015-12-26 21:06:08 -0800

    inorderTraversal recursive way

--- XProject/Level9.cpp
+++ XProject/Level9.cpp
@@ -180,6 +180,64 @@ void testRestoreIpAddresses()
         cout << s << endl;
 }
 
+#pragma mark - inorderTraversal
+//Given a binary tree, return the inorder traversal of its nodes' values.
+//
+//For example:
+//Given binary tree {1,#,2,3},
+//1
+//\
+//2
+///
+//3
+//return [1,3,2].
+//
+//Note: Recursive solution is trivial, could you do it iteratively?
+//
+//confused what "{1,#,2,3}" means? > read more on how binary tree is serialized on OJ.
+//
+//
+//OJ's Binary Tree Serialization:
+//The serialization of a binary tree follows a level order traversal, where '#' signifies a path terminator where no node exists below.
+//
+//Here's an example:
+//1
+/// \
+//2   3
+///
+//4
+//\
+//5
+//The above binary tree is serialized as "{1,2,3,#,#,4,#,#,5}".
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+void _inOrderTraversal( TreeNode* root, vector<int>& out )
+{
+    if( root == nullptr )
+        return;
+    
+    _inOrderTraversal(root->left, out );
+    out.push_back(root->val);
+    _inOrderTraversal(root->right, out );
+}
+
+vector<int> inorderTraversal(TreeNode* root)
+{
+    //In-order (symmetric)
+    //Traverse the left subtree by recursively calling the in-order function.
+    //Display the data part of current element.
+    //Traverse the right subtree by recursively calling the in-order function.
+    vector<int> out;
+    _inOrderTraversal(root, out);
+    
+    return out;
+}
+
 #pragma mark - run
 
 void Level9::Run()
