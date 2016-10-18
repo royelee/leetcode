292293106641eff8bd0f86eae942ed5dc1235b5d
Roye Li <royetest02@hotmail.com>
2016-10-18 15:36:30 -0700

    numIslands
    face time exceed because for BFS, need to mark it first otherwise some element is pushed into queue many times

--- XProject/Level16.cpp
+++ XProject/Level16.cpp
@@ -144,9 +144,124 @@ void testRightSideView()
         cout << i << ", ";
 }
 
+#pragma mark - numIslands
+//Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+//
+//Example 1:
+//
+//11110
+//11010
+//11000
+//00000
+//Answer: 1
+//
+//Example 2:
+//
+//11000
+//11000
+//00100
+//00011
+//Answer: 3
+int numIslands(vector<vector<char>>& grid)
+{
+    int founded = 0;
+    for( int i = 0; i < grid.size(); i++ )
+    {
+        for( int j = 0; j < grid[i].size(); j++ )
+        {
+            if( grid[i][j] == '1' )
+            {
+                founded++;
+                grid[i][j] = 'X';
+                
+                // Dig the island.
+                queue<pair<int, int>> q;
+                q.push(make_pair(i, j));
+                while( !q.empty() )
+                {
+                    auto currentPos = q.front();
+                    q.pop();
+                    // Go four directions
+                    if( currentPos.first - 1 >= 0 && grid[currentPos.first-1][currentPos.second] == '1' )
+                    {
+                        q.push(make_pair(currentPos.first-1, currentPos.second));
+                        grid[currentPos.first-1][currentPos.second] = 'X';
+                    }
+                    if( currentPos.first + 1 < grid.size() && grid[currentPos.first+1][currentPos.second] == '1' )
+                    {
+                        q.push(make_pair(currentPos.first+1, currentPos.second));
+                        grid[currentPos.first+1][currentPos.second] = 'X';
+                    }
+                    if( currentPos.second - 1 >= 0 && grid[currentPos.first][currentPos.second-1] == '1' )
+                    {
+                        q.push(make_pair(currentPos.first, currentPos.second-1));
+                        grid[currentPos.first][currentPos.second-1] = 'X';
+                    }
+                    if( currentPos.second + 1 < grid[0].size() && grid[currentPos.first][currentPos.second+1] == '1' )
+                    {
+                        q.push(make_pair(currentPos.first, currentPos.second+1));
+                        grid[currentPos.first][currentPos.second+1] = 'X';
+                    }
+                }
+            }
+        }
+    }
+    
+    return founded;
+}
+
+void testNumIslands()
+{
+    vector<vector<char>> t = {
+        { '1','1','0','0','0' },
+        { '1','1','0','0','0' },
+        { '0','0','1','0','0' },
+        { '0','0','0','1','1' }
+    };
+    vector<vector<char>> t1 = {{'1','0','1','1','0','1','1'}};
+    vector<vector<char>> t2 =
+    {
+        {'1','1','1'},
+        {'0','1','0'},
+        {'1','1','1'}
+    };
+    vector<string> t3s = {
+        "11111011111111101011",
+        "01111111111110111110",
+        "10111001101111111111",
+        "11110111111111111111",
+        "10011111111111111111",
+        "10111111011101110111",
+        "01111111111101101111",
+        "11111111111101111011",
+        "11111111110111111111",
+        "11111111111111111111",
+        "01111111011111111111",
+        "11111111111111111111",
+        "11111111111111111111",
+        "11111011111110111111",
+        "10111110111011110111",
+        "11111111111101111110",
+        "11111111111110111100",
+        "11111111111111111111",
+        "11111111111111111111",
+        "11111111111111111111"
+    };
+    vector<vector<char>> t3;
+    for( auto& s : t3s )
+    {
+        vector<char> cs;
+        for( auto& c : s )
+            cs.push_back(c);
+        t3.push_back(cs);
+    }
+    
+    cout << numIslands(t3);
+}
+
 #pragma mark - run
 
 void Level16::Run()
 {
-    testRightSideView();
+    testNumIslands();
 }
