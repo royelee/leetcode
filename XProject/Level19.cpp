1532fcd217b8ccd5632101042f05779b77907725
Roye Li <royetest02@hotmail.com>
2016-11-30 22:40:16 -0800

    Queue *
    ? Think about the solution use two stacks, how to do it?
    
    ===
    
    class Queue {
        stack<int> input, output;
    public:
    
        void push(int x) {
            input.push(x);
        }
    
        void pop(void) {
            peek();
            output.pop();
        }
    
        int peek(void) {
            if (output.empty())
                while (input.size())
                    output.push(input.top()), input.pop();
            return output.top();
        }
    
        bool empty(void) {
            return input.empty() && output.empty();
        }
    };

--- XProject/Level19.cpp
+++ XProject/Level19.cpp
@@ -53,6 +53,57 @@ void testIsPowerOfTwo()
         cout << pow(2,i) << endl;
 }
 
+//Implement the following operations of a queue using stacks.
+//
+//push(x) -- Push element x to the back of queue.
+//pop() -- Removes the element from in front of queue.
+//peek() -- Get the front element.
+//empty() -- Return whether the queue is empty.
+//Notes:
+//You must use only standard operations of a stack -- which means only push to top, peek/pop from top, size, and is empty operations are valid.
+//Depending on your language, stack may not be supported natively. You may simulate a stack by using a list or deque (double-ended queue), as long as you use only standard operations of a stack.
+//You may assume that all operations are valid (for example, no pop or peek operations will be called on an empty queue).
+class Queue {
+    stack<int> m_stack;
+    
+public:
+    // Push element x to the back of queue.
+    void push(int x)
+    {
+        stack<int> b;
+        
+        while( !m_stack.empty() )
+        {
+            int v = m_stack.top();
+            b.push(v);
+            m_stack.pop();
+        }
+        b.push(x);
+        
+        while( !b.empty() )
+        {
+            int v = b.top();
+            m_stack.push(v);
+            b.pop();
+        }
+    }
+    
+    // Removes the element from in front of queue.
+    void pop(void) {
+        m_stack.pop();
+    }
+    
+    // Get the front element.
+    int peek(void) {
+        return m_stack.top();
+    }
+    
+    // Return whether the queue is empty.
+    bool empty(void) {
+        return m_stack.empty();
+    }
+};
+
 #pragma mark - run
 
 void Level19::Run()
