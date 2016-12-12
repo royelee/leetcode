6983db7102f962e05cbd05ef487e8fc8b0189f75
Roye Li <royetest02@hotmail.com>
2016-12-11 21:40:28 -0800

    lowestCommonAncestor

--- XProject/Level19.cpp
+++ XProject/Level19.cpp
@@ -154,6 +154,53 @@ static void TestIsPalindrome()
     cout << BoolToStr(isPalindrome(node));
 }
 
+#pragma mark - lowestCommonAncestor
+
+//Given a binary search tree (BST), find the lowest common ancestor (LCA) of two given nodes in the BST.
+//
+//According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes v and w as the lowest node in T that has both v and w as descendants (where we allow a node to be a descendant of itself).”
+//
+//        _______6______
+//       /              \
+//    ___2__          ___8__
+//   /      \        /      \
+//   0      _4       7       9
+//         /  \
+//         3   5
+//For example, the lowest common ancestor (LCA) of nodes 2 and 8 is 6. Another example is LCA of nodes 2 and 4 is 2, since a node can be a descendant of itself according to the LCA definition.
+TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q)
+{
+    if( root == nullptr || p == nullptr || q == nullptr )
+        return nullptr;
+    
+    TreeNode* small = p->val < q->val ? p : q;
+    TreeNode* big = small == p ? q : p;
+    
+    if( small->val <= root->val && root->val <= big->val )
+        return root;
+    else if( root->val < small->val )
+        return lowestCommonAncestor(root->right, p, q);
+    else
+        return lowestCommonAncestor(root->left, p, q);
+}
+
+void testLowestCommonAncestor()
+{
+    TreeNode* root = new TreeNode(6);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(8);
+    root->left->left = new TreeNode(0);
+    root->left->right = new TreeNode(4);
+    root->left->right->left = new TreeNode(3);
+    root->left->right->right = new TreeNode(5);
+    root->right->left = new TreeNode(7);
+    root->right->right = new TreeNode(9);
+    
+    cout << ( lowestCommonAncestor(root, root->left, root->right)->val == 6 ) << endl;
+    cout << ( lowestCommonAncestor(root, root->left->right, root->left)->val == 2 ) << endl;
+    cout << ( lowestCommonAncestor(root, root->left, root->left->right)->val == 2 ) << endl;
+}
+
 #pragma mark - run
 
 void Level19::Run()
