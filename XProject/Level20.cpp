e44116995159f5c79926efc5429d0165f2b56579
Roye Li <royetest02@hotmail.com>
2016-12-15 21:42:34 -0800

    deleteNode

--- XProject/Level20.cpp
+++ XProject/Level20.cpp
@@ -66,6 +66,21 @@ static void testLowestCommonAncestor()
    cout << ( lowestCommonAncestor(root, root->left, root->left->right)->val == 2 ) << endl;
 }
 
+#pragma mark - deleteNode
+//Write a function to delete a node (except the tail) in a singly linked list, given only access to that node.
+//
+//Supposed the linked list is 1 -> 2 -> 3 -> 4 and you are given the third node with value 3, the linked list should become 1 -> 2 -> 4 after calling your function.
+void deleteNode(ListNode* node)
+{
+    if( node == nullptr )
+        return;
+    
+    ListNode* next = node->next;
+    node->val = next->val;
+    node->next = next->next;
+}
+
+
 #pragma mark - run
 
 void Level20::Run()
